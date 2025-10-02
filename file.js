b7e65f5624a93fa459b59a30504c6cdc2e51e4f8
Alfacluze <ahasanrajvi2013@gmail.com>
2025-10-01 20:48:30 -0400

    first message

---
+++ file.js
@@ -0,0 +1,2 @@
+console.log("Working on source code using Git & GitHub");
+console.log("Code sample for A3 in JavaScript");
---
+++ file.py
@@ -0,0 +1,7 @@
+print("Working on source code using Git & GitHub")
+print("Code sample for A3 in Python")
+
+# A simple calculation
+x = 5
+y = 3
+print("The sum of", x, "and", y, "is:", x + y)
